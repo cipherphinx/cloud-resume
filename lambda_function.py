c7afd2eccd45451ee0ac5647d8cfda43eef01e24
cipherphinx <cipherphinx@gmail.com>
2023-05-06 18:04:48 +0800

    commiting the updated python code

--- lambda_function.py
+++ lambda_function.py
@@ -4,20 +4,22 @@ import boto3
 dynamodb = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-
     table_name = os.environ['TABLE_NAME']
 
     response = dynamodb.update_item(
         TableName=table_name,
         Key={
-            'website':{
-                'S': 'resume.arfeljunvelasco.live'}    },
-        UpdateExpression='SET visitor_count = visitor_count + :inc',
+            'website': {
+                'S': 'resume.arfeljunvelasco.live'
+            }
+        },
+        UpdateExpression='SET visitor_count = if_not_exists(visitor_count, :zero) + :inc',
         ExpressionAttributeValues={
+            ':zero': {'N': '0'},
             ':inc': {'N': '1'}
         },
-        ReturnValues="UPDATED_NEW")
+        ReturnValues='UPDATED_NEW'
+    )
 
-    print("UPDATING ITEM")
-
-    print(response["Attributes"]["visitor_count"]["N"])
+    print('UPDATING ITEM')
+    return response
