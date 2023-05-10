7e35d80b40532d2e7745a7b7166f03278c3f4c07
cipherphinx <cipherphinx@gmail.com>
2023-05-10 17:38:54 +0800

    Added the working api_gateway.tf file

--- terraform-iaac/api_gateway.tf
+++ terraform-iaac/api_gateway.tf
@@ -7,14 +7,7 @@ resource "aws_api_gateway_rest_api" "lambda_api" {
   }
 }
 
-/*
-resource "aws_api_gateway_resource" "lambda_resource" {
-  path_part   = ""
-  parent_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-}
-*/
-
+# Creating the POST method
 resource "aws_api_gateway_method" "post_method" {
   rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
   resource_id   = aws_api_gateway_rest_api.lambda_api.root_resource_id
@@ -22,6 +15,7 @@ resource "aws_api_gateway_method" "post_method" {
   authorization = "NONE"
 }
 
+# Specifying the integration type
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.lambda_api.id
   resource_id             = aws_api_gateway_rest_api.lambda_api.root_resource_id
@@ -31,7 +25,59 @@ resource "aws_api_gateway_integration" "lambda_integration" {
   uri                     = aws_lambda_function.get_visitor_count_function.invoke_arn
 }
 
-# Lambda
+# Creating the method response
+resource "aws_api_gateway_method_response" "method_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_rest_api.lambda_api.root_resource_id
+  http_method = aws_api_gateway_method.post_method.http_method
+  status_code = "200"
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+
+}
+
+# Creating the integration response
+resource "aws_api_gateway_integration_response" "integration_response_200" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_rest_api.lambda_api.root_resource_id
+  http_method = aws_api_gateway_method.post_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [aws_api_gateway_integration.lambda_integration]
+}
+
+# Creating REST API Deployment
+resource "aws_api_gateway_deployment" "api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [aws_api_gateway_integration_response.integration_response_200]
+}
+
+# Deploying the API to stage 'v1'
+resource "aws_api_gateway_stage" "stage_v1" {
+  deployment_id = aws_api_gateway_deployment.api_deployment.id
+  rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
+  stage_name    = "v1"
+}
+
+# Adding resource policy to lambda to allow invocation
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
@@ -41,55 +87,3 @@ resource "aws_lambda_permission" "apigw_lambda" {
   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   source_arn = "arn:aws:execute-api:${var.region}:${local.accountID}:${aws_api_gateway_rest_api.lambda_api.id}/*/${aws_api_gateway_method.post_method.http_method}/"
 }
-
-resource "aws_api_gateway_deployment" "api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
-
-  /*triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.lambda_api.body))
-  }
-  */
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_api_gateway_stage" "stage_v1" {
-  deployment_id = aws_api_gateway_deployment.api_deployment.id
-  rest_api_id   = aws_api_gateway_rest_api.lambda_api.id
-  stage_name    = "v1"
-}
-
-/*
-resource "aws_lambda_function" "lambda" {
-  filename      = "lambda.zip"
-  function_name = "mylambda"
-  role          = aws_iam_role.role.arn
-  handler       = "lambda.lambda_handler"
-  runtime       = "python3.7"
-
-  source_code_hash = filebase64sha256("lambda.zip")
-}
-
-
-# IAM
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-resource "aws_iam_role" "role" {
-  name               = "myrole"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-*/
