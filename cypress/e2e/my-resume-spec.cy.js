42859b7fbee8a577eec45dd4ebb4002c59326b99
Arfel Jun Velasco <M263005@ONE.merckgroup.com>
2023-05-15 10:09:22 +0800

    added the cypress file

---
+++ cypress/e2e/my-resume-spec.cy.js
@@ -0,0 +1,41 @@
+describe('End to End test for my API', () => {
+  let visitorCount1, visitorCount2;
+
+  it('Check if visitor_count attribute is available and get its value from the API', () => {
+    cy.request('POST','https://za7vzhc1nc.execute-api.ap-southeast-1.amazonaws.com/v1')
+      .then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body).to.have.property('Attributes');
+        expect(response.body.Attributes).to.have.property('visitor_count');
+
+        visitorCount1 = response.body.Attributes.visitor_count.N;
+      });
+
+    cy.wait(1000); // wait 1 second
+
+    cy.request('POST','https://za7vzhc1nc.execute-api.ap-southeast-1.amazonaws.com/v1')
+      .then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body.Attributes).to.have.property('visitor_count');
+        visitorCount2 = response.body.Attributes.visitor_count.N;
+        
+      });
+  });
+
+  it('Check if the visitor count is updated every time the API is accessed', () => {
+    expect(parseInt(visitorCount2)).to.be.greaterThan(parseInt(visitorCount1));
+  });
+
+  it('Checks if the API is accessible via other methods', () => {
+    cy.request({
+      method: 'GET',
+      url: 'https://za7vzhc1nc.execute-api.ap-southeast-1.amazonaws.com/v1',
+      failOnStatusCode: false,
+    }).then((response) => {
+      //expect(response.status).to.eq(403);
+      expect(response.body).to.have.property('message', 'Missing Authentication Token');
+    });
+
+  });
+
+});
