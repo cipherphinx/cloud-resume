97b608d71632742ed53353e0f88fea9496c826b0
cipherphinx <cipherphinx@gmail.com>
2023-05-18 17:33:00 +0800

    Added the build test yml

--- lambda.tf
+++ lambda.tf
@@ -60,11 +60,13 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   policy_arn = aws_iam_policy.lambda_role.arn
 }
 
+/*
 data "archive_file" "lambda" {
   type        = "zip"
   source_file = "lambda_function.py"
   output_path = "lambda_function_payload.zip"
 }
+*/
 
 # Creating the lambda function
 resource "aws_lambda_function" "get_visitor_count_function" {
@@ -75,7 +77,7 @@ resource "aws_lambda_function" "get_visitor_count_function" {
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
 
-  source_code_hash = data.archive_file.lambda.output_base64sha256
+  source_code_hash = filebase64sha256("artifact/lambda_function_payload.zip")
 
   runtime = "python3.10"
 
